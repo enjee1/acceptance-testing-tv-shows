9ba61a36b0c482102c2ae7b15e43023ba7a11b42
enjee <ngiarrusso@gmail.com>
2019-05-16 14:50:02 -0400

    Update 03 spec and logic in server.rb

--- server.rb
+++ server.rb
@@ -18,9 +18,18 @@ end
 post "/television_shows" do
   @genres = TelevisionShow::GENRES
   show_details = params.values
+  CSV.foreach("television-shows.csv", headers: true) do |row|
+    show_details.each do |field|
+      
+    end
+  end
+
   if show_details.any? { |field| field == "" }
     @error_message = "Please fill in all required fields"
     erb :new
+  # elsif show_list.include?(show_details.each { |detail| detail.to_s } )
+  #   @error_message = "The show has already been added"
+  #   erb :new
   else
     CSV.open("television-shows.csv", "a") do |file|
       file << show_details
